b92f94b8b93f966c57f9365b978f2dead6d94037
9LLPPLL6 <LLP_PLL@outlook.com>
2025-05-19 07:36:32 +0000

    update

--- examples/pretrain_zero_bubble.sh
+++ examples/pretrain_zero_bubble.sh
@@ -45,21 +45,13 @@ WORLD_SIZE_IN_GPUS=$(( $WORLD_SIZE * $GPUS_PER_NODE ))
 if [ -z "$PIPELINE_SIZE" ]; then
   PIPELINE_SIZE=2
   LAYERS=8
-  MICRO_BATCH_SIZE=1
-  GLOBAL_BATCH_SIZE=8
-  HIDDEN_SIZE=8192
-  ATTENTION_HEADS=64
+  MICRO_BATCH_SIZE=4
+  GLOBAL_BATCH_SIZE=16
+  HIDDEN_SIZE=1024
+  ATTENTION_HEADS=16
   ZERO_BUBBLE_MEM_LIMIT=$((2 * $PIPELINE_SIZE))
 fi
 
-profile_ranks="0"
-for ((i = 1; i < $WORLD_SIZE_IN_GPUS; i++)); do
-    profile_ranks="$profile_ranks $i"
-done
-if [ -z "$ZERO_BUBBLE_TIMER_START" ]; then
-  ZERO_BUBBLE_TIMER_START=100
-  ZERO_BUBBLE_TIMER_END=110
-fi
 
 if [ -z "$EVAL_INTERVAL" ]; then
   EVAL_INTERVAL=10000
@@ -77,15 +69,15 @@ options=" \
   --hidden-size $HIDDEN_SIZE \
   --num-attention-heads $ATTENTION_HEADS \
   --exit-interval $EXIT_INTERVAL \
-  --seq-length 4096 \
-  --max-position-embeddings 8192 \
+  --seq-length 1024 \
+  --max-position-embeddings 1024 \
   --micro-batch-size $MICRO_BATCH_SIZE \
   --global-batch-size $GLOBAL_BATCH_SIZE \
   --group-query-attention \
   --num-query-groups $GQA \
-  --train-samples 146484375 \
-  --lr-decay-samples 126953125 \
-  --lr-warmup-samples 183105 \
+  --train-iters 500 \
+  --lr-decay-iters 320 \
+  --lr-warmup-fraction .01 \
   --lr 6.0e-5 \
   --min-lr 6.0e-6 \
   --lr-decay-style cosine \
@@ -102,17 +94,12 @@ options=" \
   --adam-beta2 0.95 \
   --init-method-std 0.006 \
   --no-barrier-with-level-1-timing \
-  --profile-step-start 15 \
-  --profile-step-end 17 \
   --untie-embeddings-and-output-weights \
   --use-legacy-models \
-  --sequence-parallel \
-  --use-flash-attn \
   --transformer-impl local \
-  --use-distributed-optimizer \
   --enable-zb-runtime \
   --no-create-attention-mask-in-dataloader \
-  --profile-ranks $profile_ranks "
+  "
 
 if [ -z "$FP32" ]; then
   options="$options --fp16"
@@ -127,16 +114,14 @@ if [ ! -z "$ZERO_BUBBLE_V_SCHEDULE" ]; then
   options="$options --zero-bubble-v-schedule "
 fi
 
-if [ ! -z "$ENABLE_ZERO_BUBBLE" ]; then
+if [  -z "$ENABLE_ZERO_BUBBLE" ]; then
   options="$options --enable-zero-bubble \
-  --zero-bubble-pipeline-timers-start-iter $ZERO_BUBBLE_TIMER_START \
-  --zero-bubble-pipeline-timers-end-iter $ZERO_BUBBLE_TIMER_END \
   --zero-bubble-max-pending-backward $ZERO_BUBBLE_MEM_LIMIT"
-  if [ -z "$FP32" ]; then
-    if [ -z "$SYNC_OPTIMIZER" ]; then
-        options="$options --enable-optimizer-post-validation"
-    fi
-  fi
+  # if [ -z "$FP32" ]; then
+  #   if [ -z "$SYNC_OPTIMIZER" ]; then
+  #       options="$options --enable-optimizer-post-validation"
+  #   fi
+  # fi
 fi
 
 if [ ! -z "$ENABLE_EXACTLY_NUMERIC_MATCH" ]; then
